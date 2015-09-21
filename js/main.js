85dcc71fb313f54c364d207ea56a306d34bcafec
unknown <html@2-00.itfactory.neta>
2015-09-21 17:55:27 +0200

    project beállítása

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Elemek kiválasztása.
+// @param selector: string a elemet kiválasztó szelektor.
+function _q( selector ) {
+    
+    // Elemek kiválasztása.
+    var elements = document.querySelectorAll( selector );
+    
+    return elements;
+    
+}
